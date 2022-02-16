ca0a0fba0fe8599d0b7819cc36be5d2d65425c3b
Aaryan Gupta <66556617+aaryan-gupta@users.noreply.github.com>
2022-02-16 21:35:39 +0530

    mini projects

--- SpeedTest.py
+++ SpeedTest.py
@@ -1,4 +1,4 @@
-from speedtest import Speedtest
+from speedtest import Speedtest # pip install speedtest-cli
 st = Speedtest()
 print("Download: ", st.download() / 1024 / 1024)
 print("Upload: ", st.upload() / 1024 / 1024)
